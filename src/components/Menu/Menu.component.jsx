3d0eace3a4cbdef52b3216b1a7163d8e23febe5d
Stoneburner Software <sean@stoneburnersoftware.com>
2019-09-23 23:21:56 -0400

    added cart-icon and cart-dropdown rendered based on redux state

--- src/components/Menu/Menu.component.jsx
+++ src/components/Menu/Menu.component.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
+
+import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import { isAdmin } from "../../utils/utils";
 
@@ -13,6 +16,7 @@ import {
 } from "./Menu.styles";
 
 const Menu = props => {
+  const hidden = props.hidden;
   const currentPath = props.history.location.pathname;
   const homePath = "/";
   const signinPath = "/signin";
@@ -51,14 +55,17 @@ const Menu = props => {
               </MenuOption>
             </React.Fragment>
           )}
+          <CartIcon />
+          {hidden ? null : <CartDropdown />}
         </ul>
       </nav>
     </MenuContainer>
   );
 };
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default withRouter(connect(mapStateToProps)(Menu));
